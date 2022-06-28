44030729da448ac17eaf18147a121b14677ba513
beyalivand4 <bey.alivand@burstsms.com>
2022-06-29 04:22:43 +1000

    Contact List 1

--- burstsms.tsx
+++ burstsms.tsx
@@ -25,6 +25,14 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { ChipProps } from '@mui/material/Chip/Chip';
 import Chip from '@mui/material/Chip';
+import Done from '@mui/icons-material/Done';
+import DoneAllRounded from '@mui/icons-material/DoneAllRounded';
+import PendingActionsOutlined from '@mui/icons-material/PendingActionsOutlined';
+import DraftsRounded from '@mui/icons-material/DraftsRounded';
+import CircleOutlined from '@mui/icons-material/CircleOutlined';
+import Pending from '@mui/icons-material/Pending';
+import MailLockOutlined from '@mui/icons-material/MailLockOutlined';
+import DraftsTwoTone from '@mui/icons-material/DraftsTwoTone';
 
 
 
@@ -53,19 +61,16 @@ function createData(
 }
 const rows = [
   createData('UX Conversation','Email cleans', 'Bey', 'Design contact list',
-  
-  <Chip label="Sent" color="primary" />),
+  <Chip label="Sent" color="success" icon={<Done/>} variant="outlined"/>),
 
 
-
-
-
-  createData('Front-end convo','Credit Card Expiry', 'Chad', 'Frontend & UX list','Completed'),
-  createData('MUI convo','Survey', 'Timo', 'Front-end guilt peeps','Sending'),
-  createData('Scrum convo','Survey', 'John', 'Kando peeps','Draft'),
-  createData('PADs','Email cleans', 'Simon.M', 'Product Team','Pending'),
-  createData('Dev talk','Survey', 'Simon.K', 'Dev contact list','Active'),
-  createData('Roadmap','Email cleans', 'Tim', 'Conversr contact list','Draft'),
+  createData('Front-end convo','Credit Card Expiry', 'Chad', 'Frontend & UX list',<Chip label="Completed" color="success" icon={<DoneAllRounded/>}/>),
+  createData('MUI convo','Survey', 'Timo', 'Front-end guilt peeps',
+  <Chip label="Sending" color="primary" icon={<Pending/>} variant="outlined"/>),
+  createData('Scrum convo','Survey', 'John', 'Kando peeps',<Chip label="Draft" color="warning" icon={<DraftsTwoTone/>} variant="outlined"/>),
+  createData('PADs','Email cleans', 'Simon.M', 'Product Team',<Chip label="Completed" color="success" icon={<DoneAllRounded/>}/>),
+  createData('Dev talk','Survey', 'Simon.K', 'Dev contact list',<Chip label="Active" color="primary" icon={<CircleOutlined/>} variant="outlined"/>),
+  createData('Roadmap','Email cleans', 'Tim', 'Conversr contact list',<Chip label="Draft" color="warning" icon={<DraftsTwoTone/>} variant="outlined"/>),
 ];
           
 export default function RecipeReviewCard() {
@@ -76,7 +81,7 @@ export default function RecipeReviewCard() {
   };
 
   return (
-    <Card sx={{ maxWidth:800
+    <Card sx={{ maxWidth:400
 
      }}>
       <CardHeader
@@ -91,7 +96,8 @@ export default function RecipeReviewCard() {
         subheader="June 28, 2022"
       />
        <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ maxWidth:1024 }} 
+      aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Name</TableCell>
